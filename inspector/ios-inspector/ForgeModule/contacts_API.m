463a7a3993df67c90fcc6f2ea522c81bfb821b67
Alex Horak <alexamh9@gmail.com>
2013-08-01 22:10:38 -0400

    Queries are now handled

--- inspector/ios-inspector/ForgeModule/contacts_API.m
+++ inspector/ios-inspector/ForgeModule/contacts_API.m
@@ -10,17 +10,10 @@
     
     int startAt = [skip intValue];
     int amtToReturn = [limit intValue];
-
-//    int amtLeft = will difer for each
-    
-    
-    // need queried address book size
     
     ABAddressBookRef addressBook = ABAddressBookCreate();
     CFArrayRef queriedAddressBook = ABAddressBookCopyPeopleWithName(addressBook,
                                                         (__bridge CFStringRef)searchQuery);
-    NSUInteger queriedAddressBookSize = CFArrayGetCount(queriedAddressBook);
-    NSLog(@"Size of Queried Array. If 0 that just means the search string is blank: %lu",(unsigned long)queriedAddressBookSize);
     
     if ([searchQuery isEqual: @""]) {
         CFArrayRef addressBookCopy = ABAddressBookCopyArrayOfAllPeople(addressBook);
@@ -36,26 +29,17 @@
         
         int amtLeft = sizeOfTotalAddressBookCopy - startAt;
         
-        NSLog(@"Total size of Address Book Copy: %i", sizeOfTotalAddressBookCopy);
-        NSLog(@"Total number of contacts still not sent over: %i", amtLeft);
-        NSLog(@"Total number of contacts I'm being asked to return: %i", amtToReturn);
-        
         if (amtLeft > 0) {
             if (amtToReturn > sizeOfTotalAddressBookCopy) {
                 amtToReturn = sizeOfTotalAddressBookCopy;
-                NSLog(@"Amt to return is larger than the address book, setting amtToReturn to size of addressbook: %i", amtToReturn);
             }
             if (amtLeft < amtToReturn) {
                 amtToReturn = amtLeft;
-                NSLog(@"amtLeft <= amtToReturn and so we have reached the end of the road. Return everything that is left: %i", amtToReturn);
             }
-
-            NSLog(@"amtLeft: %i >= amtToReturn: %i and so go on per the usual", amtLeft, amtToReturn);
             
             int stopAt = amtToReturn + startAt;
             
             for (CFIndex i = startAt; i < stopAt; i++) {
-                NSLog(@"startAt: %i stopAt: %i i: %li", startAt, stopAt, i);
                 ABRecordRef person = CFArrayGetValueAtIndex(addressBookCopy, i);
                 NSString * contactFirstName = (__bridge NSString *)ABRecordCopyValue( person, kABPersonFirstNameProperty);
                 NSString * contactLastName = (__bridge NSString *)ABRecordCopyValue( person, kABPersonLastNameProperty);
@@ -105,40 +89,60 @@
         
         NSMutableArray *matchedContacts = [[NSMutableArray alloc] init];
         
-        for (int i = startAt; i <= startAt; i++) {
-            NSString * contactFirstName = (__bridge NSString *)ABRecordCopyValue( CFArrayGetValueAtIndex(queriedAddressBook, i), kABPersonFirstNameProperty);
-            NSString * contactLastName = (__bridge NSString *)ABRecordCopyValue( CFArrayGetValueAtIndex(queriedAddressBook, i), kABPersonLastNameProperty);
-            NSMutableDictionary *contactPhoneNumbers = [[NSMutableDictionary alloc] init];
-            ABRecordRef person = CFArrayGetValueAtIndex(queriedAddressBook, i);
-            NSMutableArray *contactEmails = [[NSMutableArray alloc] init];
-            
-            ABMultiValueRef emails = ABRecordCopyValue(person, kABPersonEmailProperty);
-            for (CFIndex j=0; j < ABMultiValueGetCount(emails); j++) {
-                NSString* email = (__bridge NSString*)ABMultiValueCopyValueAtIndex(emails, j);
-                [contactEmails addObject:email];
-                CFRelease((__bridge CFTypeRef)(email));                
+        int sizeOfqueriedAddressBook = CFArrayGetCount(queriedAddressBook);
+        if (sizeOfqueriedAddressBook == 0) {
+            CFRelease(addressBook);
+            CFRelease(queriedAddressBook);
+            [task error:@"No entries in address book"];
+        }
+        
+        int amtLeft = sizeOfqueriedAddressBook - startAt;
+        
+        if (amtLeft > 0) {
+            if (amtToReturn > sizeOfqueriedAddressBook) {
+                amtToReturn = sizeOfqueriedAddressBook;
+            }
+            if (amtLeft < amtToReturn) {
+                amtToReturn = amtLeft;
             }
             
-            ABMultiValueRef phoneNumbers = ABRecordCopyValue(person, kABPersonPhoneProperty);
-            for (CFIndex j=0; j< ABMultiValueGetCount(phoneNumbers); j++) {
-                NSString* number = (__bridge NSString*)ABMultiValueCopyValueAtIndex(phoneNumbers, j);
-                NSString* label = (__bridge NSString*)ABMultiValueCopyLabelAtIndex(phoneNumbers, j);
-                [contactPhoneNumbers setObject:number forKey:label];
-                CFRelease((__bridge CFTypeRef)(number));
-                CFRelease((__bridge CFTypeRef)(label));
-            }
-            
-            NSDictionary *contact = [NSDictionary
-                                     dictionaryWithObjectsAndKeys:
-                                     contactFirstName, @"firstName",
-                                     contactLastName, @"lastName",
-                                     contactEmails, @"email",
-                                     contactPhoneNumbers,@"phone",
-                                     nil];
+            int stopAt = amtToReturn + startAt;
+        
+            for (int i = startAt; i < stopAt; i++) {
+                NSString * contactFirstName = (__bridge NSString *)ABRecordCopyValue( CFArrayGetValueAtIndex(queriedAddressBook, i), kABPersonFirstNameProperty);
+                NSString * contactLastName = (__bridge NSString *)ABRecordCopyValue( CFArrayGetValueAtIndex(queriedAddressBook, i), kABPersonLastNameProperty);
+                NSMutableDictionary *contactPhoneNumbers = [[NSMutableDictionary alloc] init];
+                ABRecordRef person = CFArrayGetValueAtIndex(queriedAddressBook, i);
+                NSMutableArray *contactEmails = [[NSMutableArray alloc] init];
+                
+                ABMultiValueRef emails = ABRecordCopyValue(person, kABPersonEmailProperty);
+                for (CFIndex j=0; j < ABMultiValueGetCount(emails); j++) {
+                    NSString* email = (__bridge NSString*)ABMultiValueCopyValueAtIndex(emails, j);
+                    [contactEmails addObject:email];
+                    CFRelease((__bridge CFTypeRef)(email));                
+                }
+                
+                ABMultiValueRef phoneNumbers = ABRecordCopyValue(person, kABPersonPhoneProperty);
+                for (CFIndex j=0; j< ABMultiValueGetCount(phoneNumbers); j++) {
+                    NSString* number = (__bridge NSString*)ABMultiValueCopyValueAtIndex(phoneNumbers, j);
+                    NSString* label = (__bridge NSString*)ABMultiValueCopyLabelAtIndex(phoneNumbers, j);
+                    [contactPhoneNumbers setObject:number forKey:label];
+                    CFRelease((__bridge CFTypeRef)(number));
+                    CFRelease((__bridge CFTypeRef)(label));
+                }
+                
+                NSDictionary *contact = [NSDictionary
+                                         dictionaryWithObjectsAndKeys:
+                                         contactFirstName, @"firstName",
+                                         contactLastName, @"lastName",
+                                         contactEmails, @"email",
+                                         contactPhoneNumbers,@"phone",
+                                         nil];
 
-            [matchedContacts addObject:contact];
-            CFRelease(emails);
-            CFRelease(phoneNumbers);
+                [matchedContacts addObject:contact];
+                CFRelease(emails);
+                CFRelease(phoneNumbers);
+            }
         }
         
         if (queriedAddressBook != nil) {
