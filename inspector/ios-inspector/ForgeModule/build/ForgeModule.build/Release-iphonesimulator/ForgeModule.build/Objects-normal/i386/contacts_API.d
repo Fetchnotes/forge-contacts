743ded524a67cfe315978d367f3c70db7eae2900
Alex Horak <alexamh9@gmail.com>
2013-09-12 19:09:03 -0400

    Added Grand Central Dispatch multi-threading and display valuable alerts

--- inspector/ios-inspector/ForgeModule/build/ForgeModule.build/Release-iphonesimulator/ForgeModule.build/Objects-normal/i386/contacts_API.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/alexhorak/forge-workspace/modules/contacts/inspector/ios-inspector/ForgeModule/contacts_API.m \
-  /Users/alexhorak/forge-workspace/modules/contacts/inspector/ios-inspector/ForgeModule/contacts_API.h
