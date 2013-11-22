fdeb8319d9cee22cd2cc12db22cd033bf216b7d6
Horak <alexamh9@gmail.com>
2013-11-22 15:00:03 -0500

    Plist allows me to set permissions' notes

---
+++ inspector/ios-inspector.2013-11-22T14-57-10.276788/ForgeModule/build/ForgeModule.build/Release-iphonesimulator/ForgeModule.build/Objects-normal/i386/contacts_API.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/horak/Fetchnotes/forge-workspace/modules/contacts/inspector/ios-inspector/ForgeModule/contacts_API.m \
+  /Users/horak/Fetchnotes/forge-workspace/modules/contacts/inspector/ios-inspector/ForgeModule/contacts_API.h \
+  /Users/horak/Fetchnotes/forge-workspace/modules/contacts/inspector/ios-inspector/ForgeModule/JBDeviceOwner.h \
+  /Users/horak/Fetchnotes/forge-workspace/modules/contacts/inspector/ios-inspector/ForgeModule/UIDevice+JBDeviceOwner.h
---
+++ inspector/ios-inspector.2013-11-22T14-57-10.276788/ForgeModule/contacts_API.h
@@ -0,0 +1,12 @@
+#import <Foundation/Foundation.h>
+
+@interface contacts_API : NSObject
+
+// There is no requirement to declare your API methods in this file,
+// unless you wish to call them from other native code.
+
+//@property (strong, nonatomic) NSString *contactFirstName;
+//
+//- (NSString*)sanitizeName:(NSString *)name;
+
+@end
